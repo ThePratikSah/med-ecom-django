fcaa025d86e3eb12eb87f32020c87c0a2d67addf
Pratik Sah <shivam3448@gmail.com>
2024-11-05 20:31:30 +0530

    Added ViewSet for all the serializers

---
+++ src/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets
+from store.models import Category, Product, Order, OrderItem
+from .serializers import (
+    CategorySerializer,
+    ProductSerializer,
+    OrderSerializer,
+    OrderItemSerializer,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
