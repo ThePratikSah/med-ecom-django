2cb57bed3e5b069523cc16391b4726b11c7da038
Pratik Sah <shivam3448@gmail.com>
2024-11-06 22:46:13 +0530

    Added URLs for register and login

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,13 +1,31 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, OrderViewSet, OrderItemViewSet
+from .views import (
+    CategoryViewSet,
+    ProductViewSet,
+    OrderViewSet,
+    OrderItemViewSet,
+    LoginView,
+    RegisterViewSet,
+)
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="categories")
 router.register(r"products", ProductViewSet, basename="products")
 router.register(r"orders", OrderViewSet, basename="orders")
 router.register(r"order-items", OrderItemViewSet, basename="order-items")
+router.register(r"register", RegisterViewSet, basename="register")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("login/", LoginView.as_view(), name="token_obtain_pair"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
