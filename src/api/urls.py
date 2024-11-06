a982c707f9b96c3eac4ef25a0d6364742f5ce2c9
Pratik Sah <shivam3448@gmail.com>
2024-11-06 22:51:15 +0530

    Removed token route as it was replaced by login

--- src/api/urls.py
+++ src/api/urls.py
@@ -10,7 +10,6 @@ from .views import (
 )
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
@@ -25,7 +24,6 @@ router.register(r"register", RegisterViewSet, basename="register")
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", LoginView.as_view(), name="token_obtain_pair"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
