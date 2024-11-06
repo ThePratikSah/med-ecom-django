fb159241034343fa37eb8cedc6d34db6cec22ce7
Pratik Sah <shivam3448@gmail.com>
2024-11-06 23:18:19 +0530

    Added permission for products API

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from store.models import Category, Product, Order, OrderItem
 from .serializers import (
@@ -21,6 +22,13 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "destroy"]:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        return [permission() for permission in permission_classes]
 
 
 class OrderViewSet(viewsets.ModelViewSet):
