f7007e2c94a4d226ed56da54c786b4c4e02af06e
Pratik Sah <shivam3448@gmail.com>
2024-11-06 22:45:55 +0530

    Added views for register and login

--- src/api/views.py
+++ src/api/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 from store.models import Category, Product, Order, OrderItem
 from .serializers import (
     CategorySerializer,
     ProductSerializer,
     OrderSerializer,
     OrderItemSerializer,
+    UserSerializer,
+    TokenSerializer,
 )
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -26,3 +31,14 @@ class OrderViewSet(viewsets.ModelViewSet):
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    http_method_names = ["post"]
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = TokenSerializer
