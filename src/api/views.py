db469af1a7c4ae39370847c47a3e29cd6f7a0b04
Pratik Sah <shivam3448@gmail.com>
2024-11-06 23:30:03 +0530

    Created a Base class for permission and used it in other viewsets

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    IsAuthenticated,
+)
 from django.contrib.auth.models import User
 from store.models import Category, Product, Order, OrderItem
 from .serializers import (
@@ -14,21 +18,23 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
+class BaseViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ["create", "update", "destroy"]:
             permission_classes = [IsAdminUser]
         else:
             permission_classes = [IsAuthenticatedOrReadOnly]
         return [permission() for permission in permission_classes]
+
+
+class CategoryViewSet(BaseViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(BaseViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
