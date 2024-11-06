a1b3c68db52c7dc8a003dc27a8e276c428eaf29c
Pratik Sah <shivam3448@gmail.com>
2024-11-06 22:45:38 +0530

    Added user serializer and token serializer

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from store.models import Category, Product, Order, OrderItem
 
@@ -48,3 +51,22 @@ class OrderSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class TokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.username
+        return token
